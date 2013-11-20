f048a292ac91bc12de056ae0bd6839621e5a0dbd
Vladimir Klimontovich <klimontovich@gmail.com>
2013-11-20 19:54:35 +0400

    Intial version

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-JavaTest1
-=========
+Test for JavaDevelopers (Level 1)
+=================================
 
-Test for Java Developers (Level 1)
+Please, take a look to interface com.getintent.interview.MonochromeScreen and it's 
+documentation. You should:
+
+  * Clone repository
+  * Implement the interface partially: only init() and drawHorizontalLine() methods should be implemented. All others would throw UnsupportedOperationException
+  * Please, use as least memory as possible. The key problem here is finding efficient memory structure
+  * Also please implement unit test for you implementation (use any library you like)
+  * Ideally, you make a fork and pull request on GitHub. Archiving you local repository and sending to us also works.
--- src/main/java/com/getintent/interview/MonochromeScreen.java
+++ src/main/java/com/getintent/interview/MonochromeScreen.java
@@ -1,11 +1,34 @@
 package com.getintent.interview;
 
 /**
- * Created with IntelliJ IDEA.
- * User: vklimontovich
- * Date: 20/11/13
- * Time: 19:29
- * To change this template use File | Settings | File Templates.
+ * The interface that represents state of monochrome screen
  */
 public interface MonochromeScreen {
+    /**
+     * Initializes the screen.
+     * @param width screen width
+     * @param height screen width
+     */
+    public void init(int width, int height);
+
+    /**
+     * Draws horizontal line.
+     * @param x1 left x coordinate of the line
+     * @param x2 right x coordinate of the line
+     * @param y y coordinate of line
+     */
+    public void drawHorizontalLine(int x1, int x2, int y);
+
+    /**
+     * Draws circle
+     * @param x x coordinates of center
+     * @param y y coordinate of center
+     * @param r radius
+     */
+    public void drawCircle(int x, int y, int r);
+
+    /**
+     * Draws line
+     */
+    public void drawLine(int x1, int y1, int x2, int y2);
 }
