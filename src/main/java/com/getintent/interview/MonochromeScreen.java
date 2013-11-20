53e68d033a14701a7176a2ed4c05d1e79d38240b
Vladimir Klimontovich <klimontovich@gmail.com>
2013-11-20 19:53:31 +0400

    Intial version

---
+++ src/main/java/com/getintent/interview/MonochromeScreen.java
@@ -0,0 +1,11 @@
+package com.getintent.interview;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vklimontovich
+ * Date: 20/11/13
+ * Time: 19:29
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MonochromeScreen {
+}
